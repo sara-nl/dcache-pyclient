a6ec459daafee5f0c46b23d44c92645f7c59cb02
Haili Hu <hailihu@gmail.com>
2026-04-23 10:35:04 +0200

    Added from-file tests for longlist and checksum

--- tests/integration/test_cli.py
+++ tests/integration/test_cli.py
@@ -12,6 +12,7 @@ import pytest
 
 
 class TestClassSystem:
+    """Test system information commands"""
 
     def test_help(self):
         """Get help"""
@@ -27,9 +28,12 @@ class TestClassSystem:
 
 
 class TestClassNamespace:
+    """Test namespace commands"""
 
-    def test_mkdir(self, target_env, testnames):
-        """Create directory on dCache"""
+    def test_mkdir_delete_dir(self, target_env, testnames):
+        """Create and delete directory on dCache"""
+
+        # Create directory recursively
         dcache_dir = target_env['homedir'] + '/' + target_env['testdir'] + '/' + testnames['tmpdir'] + '/' + testnames['subdir']
         out = subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"mkdir", dcache_dir, "--recursive"], text=True)
 
@@ -38,17 +42,15 @@ class TestClassNamespace:
         out = subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"list", dcache_dir], text=True)
         assert out == ""
 
-
-    def test_delete_dir(self,  target_env, testnames):
-        """Delete directory on dCache"""
+        # Delete directory recursively
         dcache_dir = target_env['homedir'] + '/' + target_env['testdir'] + '/' + testnames['tmpdir']
         out = subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"delete", dcache_dir, "--recursive"], text=True)
 
         assert "Deleted" in out
 
 
-    def test_mv(self, target_env, setup_data):
-        """Move file on dCache""" 
+    def test_mv_delete_file(self, target_env, setup_data):
+        """Move and delete file on dCache""" 
         # create testfile on dCache
         dcache_file = setup_data
 
@@ -63,12 +65,13 @@ class TestClassNamespace:
         out = subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"list", tmp_file], text=True)
         assert os.path.basename(tmp_file) in out
 
-        # move back
-        subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"mv", tmp_file,  dcache_file], text=True)
+        # delete file
+        out = subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"delete", tmp_file], text=True)
+        assert "Deleted" in out
 
 
-    def test_longlist_checksum_delete(self, target_env, setup_data):
-        """longlist, checksum and delete file on dCache"""
+    def test_longlist(self, target_env, setup_data, tmp_path):
+        """Longlist file(s) on dCache"""
 
         # create testfile on dCache
         dcache_file = setup_data
@@ -77,22 +80,59 @@ class TestClassNamespace:
         out = subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"longlist", dcache_file], text=True)
         assert dcache_file in out
 
+        # write file and folder to filelist
+        filelist = tmp_path / "file_list"
+        with open(filelist, "w", encoding="utf-8") as f:
+            f.write(dcache_file + "\n")
+            f.write(os.path.dirname(dcache_file))
+
+        out = subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"longlist", "--from-file", filelist], text=True)
+        assert dcache_file in out
+
+        # catch errors when neither path nor from_file are given
+        with pytest.raises(subprocess.CalledProcessError):
+            subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"longlist"], text=True)
+
+        # catch errors when both path and from_file are given
+        with pytest.raises(subprocess.CalledProcessError):
+            subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"longlist", dcache_file, "--from-file", filelist], text=True)
+
+
+    def test_checksum(self, target_env, setup_data, tmp_path):
+        """Get checksum of file(s) on dCache"""
+        # create testfile on dCache
+        dcache_file = setup_data
+
         # checksum
         out = subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"checksum", dcache_file], text=True)
         assert dcache_file in out
 
-        # # delete file
-        out = subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"delete", dcache_file], text=True)
-        assert f"Deleted: {dcache_file}" in out
+        # write file and folder to filelist
+        filelist = tmp_path / "file_list"
+        with open(filelist, "w", encoding="utf-8") as f:
+            f.write(dcache_file + "\n")
+            f.write(os.path.dirname(dcache_file))
+
+        out = subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"checksum", "--from-file", filelist], text=True)
+        assert dcache_file in out
+
+        # catch errors when neither path nor from_file are given
+        with pytest.raises(subprocess.CalledProcessError):
+            subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"checksum"], text=True)
+
+        # catch errors when both path and from_file are given
+        with pytest.raises(subprocess.CalledProcessError):
+            subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"checksum", dcache_file, "--from-file", filelist], text=True)
 
 
 class TestStaging:
+    """Test (un)staging commands"""
 
     def test_stage_unstage(self, target_env, setup_data):
         """Stage and unstage file on dCache"""
         # create testfile on dCache
         dcache_file = setup_data
-    
+
         # stage
         out = subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"stage", dcache_file], text=True)
         assert "Stage request submitted" in out
@@ -111,7 +151,7 @@ class TestStaging:
         # create testfile on dCache
         dcache_file = setup_data
         filelist = tmp_path / "file_list"
-        with open(filelist, "w") as f:
+        with open(filelist, "w", encoding="utf-8") as f:
             f.write(dcache_file)
 
         # stage
@@ -121,7 +161,7 @@ class TestStaging:
         assert "Targets: 1 file" in out
 
         # unstage
-        out = subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"unstage", "--from-file", filelist], text=True)        
+        out = subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"unstage", "--from-file", filelist], text=True)
         assert "Unstage request submitted" in out
         assert "Request URL" in out
         assert "Targets: 1 file" in out
@@ -132,22 +172,21 @@ class TestStaging:
         # create testfile on dCache
         dcache_file = setup_data
         filelist = tmp_path / "file_list"
-        with open(filelist, "w") as f:
+        with open(filelist, "w", encoding="utf-8") as f:
             f.write(dcache_file)
 
         # stage
         # Catch error when both path and from_file are given
         with pytest.raises(subprocess.CalledProcessError):
             subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'] ,"stage", dcache_file, "--from-file", filelist], text=True)
-        # Catch error when neither path nor from_file are given    
+        # Catch error when neither path nor from_file are given
         with pytest.raises(subprocess.CalledProcessError):
             subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'], "stage"], text=True)
-         
+
         # unstage
         # Catch error when both path and from_file are given
         with pytest.raises(subprocess.CalledProcessError):
             subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'], "unstage", dcache_file, "--from-file", filelist], text=True)
-        # Catch error when neither path nor from_file are given    
+        # Catch error when neither path nor from_file are given
         with pytest.raises(subprocess.CalledProcessError):
-            subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'],"unstage"], text=True)  
-   
+            subprocess.check_output(["ada-cli", "--tokenfile", target_env['tokenfile'], "--api", target_env['api'],"unstage"], text=True)
